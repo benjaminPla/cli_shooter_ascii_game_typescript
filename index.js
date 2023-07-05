4d1684893242efe325f0fd405583de52d0e35ae8
benjaminPla <benjamin.pla@7-11.com>
2023-07-06 00:29:35 +0200

    so many changes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import readline from "readline";
 
 const game = {
+  map: {
+    width: 32,
+    height: 32,
+    tiles: [],
+  },
+  player: {
+    position: {
+      x: 0,
+      y: 0,
+    },
+  },
   mapWidth: 32,
   mapHeight: 32,
   playerPosition: {
@@ -13,6 +24,27 @@ const game = {
   },
   points: 0,
   frames: 30,
+  bullets: {
+    items: [],
+    interval: 1000,
+  },
+};
+
+const createMap = () => {
+  const tiles = [];
+  for (let i = 0; i < game.map.height; i++) {
+    const row = [];
+    for (let j = 0; j < game.map.width; j++) {
+      row.push({ x: i, y: j, player: false });
+    }
+    tiles.push(row);
+  }
+  game.map.tiles = tiles;
+};
+
+const createPlayer = () => {
+  game.player.position.x = Math.floor(game.map.width / 2);
+  game.player.position.y = Math.floor(game.map.height / 2);
 };
 
 const createFood = () => {
@@ -20,7 +52,15 @@ const createFood = () => {
   game.foodPosition.y = Math.floor(Math.random() * game.mapHeight);
 };
 
+const createBullet = () => {
+  game.bullets.items.push({
+    position: { x: Math.floor(Math.random() * game.mapWidth), y: 0 },
+  });
+};
+
+createMap();
 createFood();
+createPlayer();
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -46,13 +86,17 @@ rl.input.on("keypress", (_, key) => {
     console.log("Thanks for playing.");
     process.exit(0);
   } else if (key.name === "w" && y > 0) {
-    game.playerPosition.y--;
+    // game.playerPosition.y--;
+    game.player.position.x--;
   } else if (key.name === "s" && y < game.mapHeight - 1) {
-    game.playerPosition.y++;
+    // game.playerPosition.y++;
+    game.player.position.x++;
   } else if (key.name === "d" && x < game.mapWidth - 1) {
-    game.playerPosition.x++;
+    // game.playerPosition.x++;
+    game.player.position.y++;
   } else if (key.name === "a" && x > 0) {
-    game.playerPosition.x--;
+    // game.playerPosition.x--;
+    game.player.position.y--;
   }
 });
 
@@ -61,32 +105,74 @@ const drawMap = () => {
 
   const player = "O";
   const food = "X";
+  const bullet = "V";
+
+  const wallVertical = "|";
+  const wallHorizontal = "T";
+
+  // game.bullets.items.forEach((bullet, index) => {
+  // if (bullet.position.y >= game.mapHeight) {
+  // game.bullets.items.splice(index, 1);
+  // }
+  // bullet.position.y++;
+  // });
 
   const createMap = () => {
-    let map = "";
-    map += `${"T".repeat(game.mapWidth + 2)}\n`;
-    for (let i = 0; i < game.mapHeight; i++) {
-      let row = "";
-      for (let j = 0; j < game.mapWidth; j++) {
-        if (i === game.foodPosition.y && j === game.foodPosition.x) {
-          row += food;
-        } else if (i === game.playerPosition.y && j === game.playerPosition.x) {
-          row += player;
-        } else {
-          row += " ";
+    // let map = "";
+    // map += `${"T".repeat(game.mapWidth + 2)}\n`;
+    // for (let i = 0; i < game.mapHeight; i++) {
+    // let row = "";
+    // for (let j = 0; j < game.mapWidth; j++) {
+    // if (i === game.foodPosition.y && j === game.foodPosition.x) {
+    // row += food;
+    // } else if (i === game.playerPosition.y && j === game.playerPosition.x) {
+    // row += player;
+    // } else {
+    // let isBulletPresent = false;
+    // game.bullets.items.forEach((bullet) => {
+    // if (bullet.position.x === j && bullet.position.y === i) {
+    // row += bullet;
+    // isBulletPresent = true;
+    // }
+    // });
+    // if (!isBulletPresent) {
+    // row += " ";
+    // }
+    // }
+    // }
+    // map += `|${row}|\n`;
+    // }
+    // map += `${"T".repeat(game.mapWidth + 2)}\n`;
+    const map = game.map.tiles.map((row, rowIndex) => {
+      const x = row.map((tile, tileIndex) => {
+        // if (tile.player) {
+        if (
+          game.player.position.x === rowIndex &&
+          game.player.position.y === tileIndex
+        ) {
+          return player;
         }
-      }
-      map += `|${row}|\n`;
-    }
-    map += `${"T".repeat(game.mapWidth + 2)}\n`;
-    return map;
+        // if (tile.y === game.map.width - 1) {
+        // return "\n";
+        // }
+        if (tile.x === 0 || tile.x === game.map.height - 1) {
+          return wallHorizontal;
+        }
+        if (tile.y === 0 || tile.y === game.map.width - 1) {
+          return wallVertical;
+        }
+        return " ";
+      });
+      return x.join("");
+    });
+    return map.join("\n");
   };
 
   process.stdout.write(createMap());
-  process.stdout.write(JSON.stringify({ game }));
+  // process.stdout.write(JSON.stringify({ game }));
 };
 
-const x = () => {
+const eat = () => {
   if (
     game.playerPosition.x === game.foodPosition.x &&
     game.playerPosition.y === game.foodPosition.y
@@ -96,9 +182,11 @@ const x = () => {
   }
 };
 
-drawMap();
-
 setInterval(() => {
   drawMap();
-  x();
+  eat();
 }, 1000 / game.frames);
+
+// setInterval(() => {
+// createBullet();
+// }, game.bullets.interval);
